0748ec51df4d4e8d6b49b093569cba4254ea89be
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 18:04:27 +0100

    feat: added toolbar and card

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar'
 @NgModule({
   declarations: [
     CoursesComponent
@@ -12,7 +13,9 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
