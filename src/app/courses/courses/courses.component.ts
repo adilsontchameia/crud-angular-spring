b3c81a5ab77feabfb3f2ff62a45609c11f58351d
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 17:55:19 +0100

    feat: mocked courses to show on table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' },
+
+    { _id: '2', name: 'Spring', category: 'back-end' }
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    //this.courses = [];
+  }
+
+  ngOnInit(): void {
+
+  }
 }
