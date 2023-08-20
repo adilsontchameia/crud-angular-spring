624ca8f7a891945d9d997c6703c3d53394b2ce23
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 19:08:46 +0100

    feat: given another name to show as observable

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,13 +11,13 @@ import { CoursesService } from '../services/courses.service';
 export class CoursesComponent {
 
   //1 courses: Course[];
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
 
   constructor(private coursesService: CoursesService) {
     //1 this.courses = [];
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {
