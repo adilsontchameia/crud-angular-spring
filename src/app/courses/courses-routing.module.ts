b0b78fda822b59eaabde67ef7b508ebf12b3bf59
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 17:03:03 +0100

    feat: lazy loading and routing

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Caminho
+  { path: '', component: CoursesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
