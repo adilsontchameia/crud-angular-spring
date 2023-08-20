f400c02ac5556537f6b39be772f387a0743b567c
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 19:07:15 +0100

    feat: progress indicator

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 
 
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule
   ]
 })
 export class AppMaterialModule { }
