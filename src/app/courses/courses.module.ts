b4a8268a97abccb8fc937cdf23e465862449ba0e
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 16:53:23 +0100

    feat: updated components

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoursesComponent
+  ],
   imports: [
     CommonModule,
     CoursesRoutingModule
