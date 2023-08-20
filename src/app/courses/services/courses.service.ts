9c98945d571b3c8a54ee69fb7c36d21c5d6a8fea
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-20 19:06:59 +0100

    feat: getting info from json mocked

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
   //Chamada asyncrona no servidor
+  private readonly API = '/assets/courses.json'; //endpoint
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end' },
-
-      { _id: '2', name: 'Spring', category: 'back-end' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+      //Forma de usar e transformar os dados
+      .pipe(
+        first(),
+        delay(6000),
+        tap(courses => console.log(courses))
+      );
   }
 }
